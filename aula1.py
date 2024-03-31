b6c2f2efcb9448a37c93a5df4fa070e482c84b46
Kerry Muniz <kerrym877@gmail.com>
2024-03-30 22:25:59 -0300

    continuação curso de python 3. Tipos de dados no python

---
+++ aula1.py
@@ -0,0 +1,17 @@
+# o sep é um separador dos argumentos
+print(12, 34, sep="-")
+
+# Tipos de dados no python
+
+#string -> tudo o que está dentro de aspas, conjunto de caracteres
+print('Oi!')
+
+#int -> números inteiros
+print(10)
+
+#float -> números com casas decimais
+print(15.6)
+
+#boolean -> valores true ou false
+print(10 == 10)
+print(10 == 11)
