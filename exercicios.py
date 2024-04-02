af68201fa0a83a6bebbbcba8220e4b005c8df3f2
kerrymuniz <132500154+kerrymuniz@users.noreply.github.com>
2024-04-01 22:03:19 -0300

    Update exercicios.py

--- exercicios.py
+++ exercicios.py
@@ -1,20 +1,54 @@
 #Exercício 1 - Cálculo IMC (peso/altura**2)
-nome = input("Digite seu nome: ")
-peso = input("Digite seu peso(kg): ")
-altura = input("Digite sua altura(m): ")
-imc = float(peso) / float(altura)**2
-    #esse f é de formatação de strings
-if(imc >= 18.5 and imc <= 24.9):
-    resultado = f'{nome} tem {float(altura):.2f} de altura, pesa {float(peso)}kg e seu Índice de Massa Corporal está normal.'
-    print("Resultado: ",imc)
-    print(resultado)
-elif(imc < 18.5):
-    resultado = f'{nome} tem {float(altura):.2f} de altura, pesa {float(peso)}kg e seu Índice de Massa Corporal está abaixo do normal.'
-    print("Resultado: ",imc)
-    print(resultado)
-else:
-    resultado = f'{nome} tem {float(altura):.2f} de altura, pesa {float(peso)}kg e seu Índice de Massa Corporal está elevado.'
-    print("Resultado: ",imc)
-    print(resultado)
+# nome = input("Digite seu nome: ")
+# peso = input("Digite seu peso(kg): ")
+# altura = input("Digite sua altura(m): ")
+# imc = float(peso) / float(altura)**2
+#     #esse f é de formatação de strings
+# if(imc >= 18.5 and imc <= 24.9):
+#     resultado = f'{nome} tem {float(altura):.2f} de altura, pesa {float(peso)}kg e seu Índice de Massa Corporal está normal.'
+#     print("Resultado: ",imc)
+#     print(resultado)
+# elif(imc < 18.5):
+#     resultado = f'{nome} tem {float(altura):.2f} de altura, pesa {float(peso)}kg e seu Índice de Massa Corporal está abaixo do normal.'
+#     print("Resultado: ",imc)
+#     print(resultado)
+# else:
+#     resultado = f'{nome} tem {float(altura):.2f} de altura, pesa {float(peso)}kg e seu Índice de Massa Corporal está elevado.'
+#     print("Resultado: ",imc)
+#     print(resultado)
 
-#Exercício 2 - Retornar o maior nº numa lista de números
+
+#Exercício 2 - Retornar o maior nº de uma lista de números
+# def maiorNumero(a, b):
+#     if(a > b):
+#         return a
+#     else:
+#         return b
+    
+# def listaNumeros(lista):
+#     maior_numero = 0
+#     for numero in lista:
+#         maior_numero = maiorNumero(maior_numero, numero)
+#     return maior_numero
+
+# lista_numeros = [0, 5, 15, 34, 7, 46, 55, 23, 75]
+# print("Os números da lista são", 0, 5, 15, 34, 7, 46, 55, 23, "e", 75)
+# print("O maior número da lista é", listaNumeros(lista_numeros))
+
+
+#Exercício 3 - Função que retorna uma lista de números pares da lista de entrada
+
+def numerosPares(dividendos):
+    for num in dividendos:
+        num_pares = num % 2
+        if(num_pares == 0):
+            num_pares = num
+        dividendos = num_pares
+    return dividendos
+
+# def listaEntrada(listaPar):
+#     lista1 = [2, 4, 5, 11, 7, 26, 35]
+    
+    
+listaNumeros = [2, 4, 5, 11, 7, 26, 35]
+print(numerosPares(listaNumeros))
